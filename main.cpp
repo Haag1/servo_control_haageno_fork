f37f32da6a71ef3b7a47aecb7cd60799fe8217a5
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2025-02-18 15:45:15 +0100

    update

--- main.cpp
+++ main.cpp
@@ -11,7 +11,8 @@
 
 using namespace threepp;
 
-auto createSprite(const std::shared_ptr<Texture> &texture) {
+auto createSprite(const std::shared_ptr<Texture>& texture)
+{
     auto spriteMaterial = MeshBasicMaterial::create({{"map", texture}});
     auto sprite = Mesh::create(PlaneGeometry::create(), spriteMaterial);
     sprite->scale.set(2, 2, 1);
@@ -19,27 +20,28 @@ auto createSprite(const std::shared_ptr<Texture> &texture) {
     return sprite;
 }
 
-class PanTiltMechanism : public Group {
+class PanTiltMechanism : public Group
+{
 public:
-    PanTiltMechanism(): camera_(60, 1, 0.01, 100) {
+    PanTiltMechanism(): camera_(60, 1, 0.01, 100)
+    {
         camera_.position.x = 0.03;
         camera_.rotateY(math::degToRad(-90));
-        camera_.rotateZ(math::degToRad(180));
 
         OBJLoader loader;
         const auto bottom = loader.load("data/pantilt/bottom.obj");
         const auto upper = loader.load("data/pantilt/upper.obj");
         const auto servo = loader.load("data/pantilt/servo.obj");
 
-        bottom->position.y = 0.9;
-        bottom->rotateZ(math::degToRad(90));
+        bottom->position.x = 0.66;
+        bottom->position.y = 0.75;
         bottom->scale *= 10;
         upper->name = "upper";
 
         auto material = MeshStandardMaterial::create();
         material->color = Color::orange;
-        auto baseGeometry = CylinderGeometry::create(0.5, 0.5, 0.5);
-        baseGeometry->applyMatrix4(Matrix4().setPosition(0, 0.25, 0));
+        auto baseGeometry = BoxGeometry::create(0.6, 1, 0.6);
+        baseGeometry->applyMatrix4(Matrix4().setPosition(0, 0.5, 0));
         auto base = Mesh::create(baseGeometry, material);
 
         add(base);
@@ -49,35 +51,39 @@ public:
         upper->add(camera_);
     }
 
-    void setTilt(float angle) {
-        getObjectByName("upper")->rotation.z = angle;
+    void setTiltSpeed(float rad)
+    {
+        tiltSpeed_ = rad;
     }
 
-    void setPan(float angle) {
-        children.front()->rotation.y = angle;
+    void setPanSpeed(float rad)
+    {
+        panSpeed_ = rad;
     }
 
-    PerspectiveCamera &getCamera() {
+    void update(float delta)
+    {
+        //tilt
+        getObjectByName("upper")->rotation.z += std::clamp(tiltSpeed_ * delta, -maxSpeed_, maxSpeed_);
+        //pan
+        children.front()->rotation.y += std::clamp(panSpeed_ * delta, -maxSpeed_, maxSpeed_);
+    }
+
+    PerspectiveCamera& getCamera()
+    {
         return camera_;
     }
 
 private:
+    float panSpeed_{0};
+    float tiltSpeed_{0};
+
+    float maxSpeed_{0.1};
     PerspectiveCamera camera_;
 };
 
-int main() {
-    Canvas canvas("Servo control");
-    GLRenderer renderer(canvas.size());
-    renderer.autoClear = false;
-
-    Scene scene;
-
-    const auto light = DirectionalLight::create();
-    scene.add(light);
-
-    PerspectiveCamera camera(60, canvas.aspect(), 0.1, 1000.);
-    camera.position.z = -5;
-
+void setBackground(Scene& scene)
+{
     std::filesystem::path path("data/Bridge2");
     std::array<std::filesystem::path, 6> urls{
         // clang-format off
@@ -90,6 +96,25 @@ int main() {
     CubeTextureLoader cubeTextureLoader{};
     auto reflectionCube = cubeTextureLoader.load(urls);
     scene.background = reflectionCube;
+}
+
+int main()
+{
+    Canvas canvas("Servo control");
+    GLRenderer renderer(canvas.size());
+    renderer.autoClear = false;
+
+    Scene scene;
+    setBackground(scene);
+
+    const auto light = HemisphereLight::create();
+    scene.add(light);
+
+    PerspectiveCamera camera(60, canvas.aspect(), 0.1, 1000.);
+    camera.position.z = -5;
+
+    auto grid = GridHelper::create();
+    scene.add(grid);
 
     PanTiltMechanism panTilt;
     scene.add(panTilt);
@@ -99,20 +124,17 @@ int main() {
 
     OrbitControls controls(camera, canvas);
 
-    auto grid = GridHelper::create();
-    scene.add(grid);
-
-    Scene orthoScene;
-    OrthographicCamera orthoCamera(-1, 1, 1, -1, 1, 10);
-    orthoCamera.position.z = 1;
-
     cv::namedWindow("PanTilt", cv::WINDOW_NORMAL);
 
     Clock clock;
-    canvas.animate([&] {
-        const auto sine = 45.f * std::sin(math::TWO_PI * 0.1f + clock.getElapsedTime() + 0);
-        panTilt.setPan(math::degToRad(sine));
-        panTilt.setTilt(math::degToRad(90));
+    canvas.animate([&]
+    {
+        const auto dt = clock.getDelta();
+        panTilt.update(dt);
+
+        const auto sine = 45.f * std::sin(math::TWO_PI * 0.1f * clock.elapsedTime + 0);
+        panTilt.setPanSpeed(math::degToRad(sine));
+        // panTilt.setTiltSpeed(math::degToRad(sine));
 
         renderer.clear();
         cameraHelper->visible = false;
@@ -132,7 +154,6 @@ int main() {
         // Convert from RGB to BGR (since OpenCV expects BGR by default)
         cv::cvtColor(image, image, cv::COLOR_RGB2BGR);
         cv::imshow("PanTilt", image);
-
     });
 
     return 0;
